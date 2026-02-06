d03dd2eb69fe455abcd138543e04aa7362f68877
Davidsea-z <genspark_dev@genspark.ai>
2026-02-06 06:46:37 +0000

    修正标题：MICROCONNET AI 改为 MICROCONNECT AI

--- .wrangler/tmp/dev-VL92e5/pages-shim.js
+++ .wrangler/tmp/dev-VL92e5/pages-shim.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-CbQbGN/strip-cf-connecting-ip-header.js
+// .wrangler/tmp/bundle-KTn0rL/strip-cf-connecting-ip-header.js
 function stripCfConnectingIPHeader(input, init) {
   const request = new Request(input, init);
   request.headers.delete("CF-Connecting-IP");
@@ -65,7 +65,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-CbQbGN/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-KTn0rL/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -97,7 +97,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-CbQbGN/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-KTn0rL/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
